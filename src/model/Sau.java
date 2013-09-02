85ebfdf8bdd359e87527d5df9658021fdb761da6
tOgg1 <tormod.haugland@gmail.com>
2013-09-02 11:57:09 +0200

    First commit

---
+++ src/model/Sau.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mayacat
+ * Date: 9/2/13
+ * Time: 11:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sau {
+
+}

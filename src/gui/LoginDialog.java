5fff63e30711a6b3ba3bc5b0e06560dc335553b2
tOgg1 <tormod.haugland@gmail.com>
2013-09-02 12:41:52 +0200

    La til baseklasser.

---
+++ src/gui/LoginDialog.java
@@ -0,0 +1,11 @@
+package gui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mayacat
+ * Date: 9/2/13
+ * Time: 12:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginDialog {
+}
---
+++ src/net/Server.java
@@ -0,0 +1,13 @@
+package net;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mayacat
+ * Date: 9/2/13
+ * Time: 12:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server
+{
+
+}

38a15b62a2c9805a517a12b2d67bb2f8f634a8ba
tOgg1 <tormod.haugland@gmail.com>
2013-09-09 14:13:34 +0200

    Created database shell

--- src/db/DatabaseHandler.java
+++ src/db/DatabaseHandler.java
@@ -1,5 +1,45 @@
 package db;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * DatabaseHandler class, deals with getting and setting data
+ *
+ */
 public class DatabaseHandler {
+
+    private Connection db;
+
+    public DatabaseHandler(){
+        try{
+            // Establish a ssh-connection to our remote server
+            final JSch jsch = new JSch();
+            Session session =  jsch.getSession(DatabaseSettings.remote_user, DatabaseSettings.remote_host, 22);
+            session.setPassword(DatabaseSettings.remote_pw);
+
+            final Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+
+            session.connect();
+            session.setPortForwardingL(3306, DatabaseSettings.remote_host, 3306);
+
+            //Connect to the database
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db = DriverManager.getConnection(DatabaseSettings.db_url, DatabaseSettings.db_user, DatabaseSettings.db_pw);
+
+        }  catch(ClassNotFoundException  | InstantiationException | IllegalAccessException | JSchException | SQLException e){
+            Log.e("Database", "You suck");
+        }
+    }
+
 
 }
---
+++ src/db/DatabaseSettings.java
@@ -0,0 +1,18 @@
+package db;
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mayacat
+ * Date: 9/9/13
+ * Time: 1:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DatabaseSettings {
+    public static String remote_host = "151.236.216.174:22";
+    public static String db_url = "jdbc:mysql://localhost:3306/poni";
+
+    public static String db_user = "ninja";
+    public static String db_pw = "sheep";
+
+    public static String remote_user = "ninja";
+    public static String remote_pw = "sheep";
+}

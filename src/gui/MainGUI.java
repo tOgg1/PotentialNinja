8b1db4ee91f78ef8deb603b21e902c5f2cd25448
Kumiii <Kumii@broadpark.no>
2013-10-14 13:55:44 +0200

    Prøver igjen

---
+++ src/gui/MainGUI.java
@@ -0,0 +1,17 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+
+public class MainGUI {
+	public static Velkommen vel = new Velkommen();
+
+	public static void main(){
+		vel.setVisible(true);
+		
+	}
+
+}
